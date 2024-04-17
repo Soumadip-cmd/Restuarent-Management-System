435731414a45e69454b30180d532a2681cea6141
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-17 20:44:27 +0530

    changes

--- src/components/FoodItem/PrevNext.js
+++ src/components/FoodItem/PrevNext.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const PrevNext = () => {
-    return (
-        <>
-            <div className="btn-group" role="group" aria-label="Basic outlined example">
+  return (
+    <>
+      {/* <div className="btn-group" role="group" aria-label="Basic outlined example">
             <button type="button" className="btn btn-outline-dark">
                 &#11164;
               </button>
@@ -13,9 +14,28 @@ const PrevNext = () => {
               <button type="button" className="btn btn-outline-dark">
                 &#11166;
               </button>
-            </div>
-        </>
-    )
-}
+            </div> */}
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <Link className=" page-link  btn btn-outline-dark" to="/" role="button">
+              &#11164;
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link btn btn-outline-dark " to="/">
+              1
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className=" page-link  btn btn-outline-dark" to="/" role="button">
+              &#11166;
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default PrevNext
+export default PrevNext;
